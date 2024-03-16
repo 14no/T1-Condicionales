0fe2c5c1a5ee2480fa91095302ce1e606fdc9885
c-catorceno <162398916+c-catorceno@users.noreply.github.com>
2024-03-16 10:05:06 -0400

    prueba comp

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -75,12 +75,8 @@ void exercise_6(int number) {
 
 void exercise_7(double r) {
   // TODO: YOUR CODE HERE
-  cin >> r;
-  if (r < 0){
-    cout << "Error: Radius cannot be negative." << endl;
-  } else {
-    cout << 4 * 3, 14 * r * r << endl;
-  }
+  
+  
 }
 
 void exercise_8(long int seconds) {
