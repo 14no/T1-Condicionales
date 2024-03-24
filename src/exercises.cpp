2b50ee1dfd6c51681bdbd68b2843a6db18541acb
c-catorceno <162398916+c-catorceno@users.noreply.github.com>
2024-03-24 08:49:06 -0400

    P4:E11

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -87,10 +87,10 @@ string exercise_11(int number) {
   // TODO: YOUR CODE HERE
   string msg;
   if (number == 11235813) { 
-    msg = "Se encontró Fibonacci.";
+    msg = "Se encontro Fibonacci.";
     cout << msg << endl;
   } else {
-    msg = "Esto no es Fibonacci.";
+    msg = "Esto no es de Fibonacci.";
     cout << msg << endl;
   }
   return msg;
