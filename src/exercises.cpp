5e837fb3ef22558612f9d35f7fcaf37d81323fc4
c-catorceno <162398916+c-catorceno@users.noreply.github.com>
2024-03-24 15:58:39 -0400

    P5:E6

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -76,29 +76,21 @@ void exercise_5(char character) {
 void exercise_6(int number) {
   // TODO: YOUR CODE HERE
   switch (number) {
-    case 1:
-    cout << "Monday" << endl;
+    case 1: cout << "Monday" << endl;
     break;
-    case 2:
-    cout << "Tuesday" << endl;
+    case 2: cout << "Tuesday" << endl;
     break;
-    case 3:
-    cout << "Wednesday" << endl;
+    case 3: cout << "Wednesday" << endl;
     break;
-    case 4:
-    cout << "Thursday" << endl;
+    case 4: cout << "Thursday" << endl;
     break;
-    case 5:
-    cout << "Friday" << endl;
+    case 5: cout << "Friday" << endl;
     break;
-    case 6:
-    cout << "Saturday" << endl;
+    case 6: cout << "Saturday" << endl;
     break;
-    case 7:
-    cout << "Sunday" << endl;
+    case 7: cout << "Sunday" << endl;
     break;
-    default:
-    cout << "Invalid input" << endl;
+    default: cout << "Invalid input" << endl;
   }
 }
 
