ef281c7785b7374f8d57ba7c8f279e2ed407e7fb
c-catorceno <162398916+c-catorceno@users.noreply.github.com>
2024-03-24 14:40:22 -0400

    P5:E14

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -152,9 +152,7 @@ string exercise_14(int number_of_docs) {
   } else if (number_of_docs == 1) {
     return "Se encontro un documento";
   } else if (number_of_docs >= 2) {
-    string number = to_string(number_of_docs);
-    string result = msg + " documentos encontrados"s;
-    return result;
+    return to_string(number_of_docs)+" documentos encontrados"s;
   } 
 }
 
