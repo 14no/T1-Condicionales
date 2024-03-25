adee5b5f03fe95d4901abd15bbeb8f66f7d419bc
c-catorceno <162398916+c-catorceno@users.noreply.github.com>
2024-03-24 22:43:17 -0400

    P7:E16

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -210,8 +210,8 @@ void exercise_16(int debut, int fin) {
   } else if (0 <= debut && debut <= 24 && 0 <= fin && fin <= 24) {
     if (0 <= debut && debut <= 7 && 0 <= fin && fin <= 7 || 17 <= debut && debut <= 24 && 17 <= fin && fin <= 24) {
       int horas = fin - debut;
-      cout << "Has alquilado una bicicleta por" << endl;
-      cout << horas << "hora(s) con el tarifario de 1 boliviano(s)" << endl;
+      cout << "Haz alquilado una bicicleta por" << endl;
+      cout << horas << " hora(s) con el tarifario de 1 boliviano(s)" << endl;
       cout << "El monto total a pagar es de " << horas << " boliviano(s)." << endl;
     } else if (7 <= debut && debut <= 17 && 7 <= fin && fin <= 17) {
       int horas = fin - debut;
